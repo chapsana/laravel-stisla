dca9901750be6d899d8e9861a7cbd2913aae18af
Alpha Olomi <alphaolomi@gmail.com>
2021-03-30 15:13:15 +0300

    feat(core):add datatable support

--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Yajra\DataTables\DataTablesServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -227,6 +228,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+//        Packages Aliases
+        'Datatables' => Yajra\DataTables\Facades\DataTables::class,
 
     ],
 
